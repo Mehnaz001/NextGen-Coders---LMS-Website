e022eb4ca3eed8155177364fee26a9d110e0ee67
Mehnaz Sheikh <165507214+Mehnaz001@users.noreply.github.com>
2026-03-14 10:15:54 +0530

    Update CORS origin to production URL

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://next-gen-coders-lms-website-g8a1.vercel.app/",
     credentials:true
 }))
 
